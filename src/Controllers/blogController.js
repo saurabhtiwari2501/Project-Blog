c45cd8b59101f846ba751a19ce2882a4c1143336
saurabhtiwari2501 <saurabhtiwari2501@gmail.com>
2022-09-05 23:11:50 +0530

    normal update

--- src/Controllers/blogController.js
+++ src/Controllers/blogController.js
@@ -4,31 +4,31 @@ const { modelName } = require("../models/authorModel")
 // _____________________________POST BLOG__________________________
 
 
-const POSTBlog = async function (req,res) {
+const POSTBlog = async function (req, res) {
 
-  try { 
+  try {
     let data = req.body
     let savedBlog = await modelName.create(data)
-    res.status(201).send({msg : savedBlog})
-}
-catch(err){
+    res.status(201).send({ msg: savedBlog })
+  }
+  catch (err) {
     res.status(500).send(err.massage)
-}
+  }
 }
 
 // ________________________________GET BLOG_________________________________
 
 
-const GETBlog = async function (req,res) {
-    
-   try{
-     let data = await modelName.find()
-    res.status(201).send({msg : data}) 
-}
-catch(err){
+const GETBlog = async function (req, res) {
+
+  try {
+    let data = await modelName.find()
+    res.status(201).send({ msg: data })
+  }
+  catch (err) {
     res.status(500).send(err.massage)
-}
+  }
 }
 
 
-module.expost= {POSTBlog,GETBlog}
+module.expost = { POSTBlog, GETBlog }
